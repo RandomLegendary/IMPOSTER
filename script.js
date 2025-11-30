fe8c1ecb22f00066af0f15bc26a21d799b6f976e
Aidan Arijs <aidan.arijs@gmail.com>
2025-11-30 13:16:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,555 @@
+const woorden = {
+  "appel": "fruit",
+  "banaan": "tropisch",
+  "sinaasappel": "citrus",
+  "druif": "tros",
+  "citroen": "zuur",
+  "watermeloen": "zomer",
+  "mango": "exotisch",
+  "kiwi": "vezels",
+  "perzik": "zacht",
+  "peer": "boom",
+  "tomaat": "nachtshade",
+  "wortel": "beta",
+  "aardappel": "zetmeel",
+  "ui": "tranen",
+  "knoflook": "aroma",
+  "spinazie": "blad",
+  "broccoli": "kool",
+  "bloemkool": "wit",
+  "paprika": "capsicum",
+  "komkommer": "hydratatie",
+  "aubergine": "paars",
+  "courgette": "groente",
+  "champignon": "schimmel",
+  "erwt": "peul",
+  "kikkererwt": "hummus",
+  "linzen": "eiwit",
+  "rijst": "graan",
+  "pasta": "italiaans",
+  "brood": "gluten",
+  "kaas": "zuivel",
+  "melk": "calcium",
+  "yoghurt": "ferment",
+  "ei": "proteïne",
+  "kip": "gevogelte",
+  "rundvlees": "rood",
+  "varkensvlees": "ham",
+  "zalm": "omega",
+  "tonijn": "blik",
+  "kabeljauw": "witvis",
+  "makreel": "vet",
+  "haring": "hollands",
+  "forel": "rivier",
+  "garnalen": "schaal",
+  "krab": "klauw",
+  "kreeft": "premium",
+  "inktvis": "tentakel",
+  "octopus": "armen",
+  "soja": "bonen",
+  "tofu": "vegan",
+  "tempeh": "gefermenteerd",
+  "suiker": "zoet",
+  "zout": "mineraal",
+  "peper": "kruid",
+  "oregano": "mediterraan",
+  "basilicum": "aroma",
+  "kaneel": "warmte",
+  "gember": "wortel",
+  "kurkuma": "geel",
+  "kardemom": "specerij",
+  "vanille": "aroma",
+  "chocolade": "cacao",
+  "karamel": "suiker",
+  "honing": "bijen",
+  "jam": "spread",
+  "koffie": "cafeïne",
+  "thee": "infusie",
+  "cola": "frisdrank",
+  "limonade": "siropen",
+  "bier": "gerst",
+  "wijn": "druiven",
+  "whisky": "distillatie",
+  "rum": "suikerriet",
+  "vodka": "graan",
+  "whiskey": "eik",
+  "stoel": "zitten",
+  "tafel": "meubel",
+  "bank": "sofa",
+  "kast": "opslag",
+  "bed": "slapen",
+  "dekbed": "warmte",
+  "kussen": "hoofden",
+  "lamp": "licht",
+  "plafond": "boven",
+  "vloer": "onder",
+  "tapijt": "zacht",
+  "gordijn": "stof",
+  "bureau": "werk",
+  "plank": "houten",
+  "spiegel": "reflectie",
+  "klok": "tijd",
+  "horloge": "pols",
+  "telefoon": "mobiel",
+  "laptop": "computer",
+  "tablet": "scherm",
+  "toetsenbord": "typen",
+  "muis": "cursor",
+  "televisie": "scherm",
+  "radio": "audio",
+  "speaker": "geluid",
+  "microfoon": "inspraak",
+  "camera": "optiek",
+  "koelkast": "koeling",
+  "vriezer": "ijs",
+  "oven": "bakken",
+  "fornuis": "vlam",
+  "pannen": "koken",
+  "magnetron": "golven",
+  "waterkoker": "koken",
+  "toaster": "roosteren",
+  "blender": "mixen",
+  "keukenmes": "snijden",
+  "snijplank": "ondergrond",
+  "pan": "braad",
+  "douche": "stralen",
+  "kraan": "water",
+  "toilet": "spoelen",
+  "zeep": "reiniging",
+  "shampoo": "haar",
+  "tandpasta": "glazuur",
+  "handdoek": "droog",
+  "bad": "liggen",
+  "wasbak": "spoel",
+  "school": "leren",
+  "klas": "groep",
+  "docent": "onderwijs",
+  "student": "discipel",
+  "universiteit": "academisch",
+  "ziekenhuis": "zorg",
+  "arts": "dokter",
+  "verpleegkundige": "zorg",
+  "apotheek": "medicatie",
+  "supermarkt": "boodschappen",
+  "winkel": "verkoop",
+  "markt": "kraam",
+  "park": "groen",
+  "bos": "bomen",
+  "strand": "zand",
+  "duin": "heuvel",
+  "berg": "top",
+  "heuvel": "hoogte",
+  "vallei": "dal",
+  "rivier": "stroming",
+  "meer": "stilte",
+  "eiland": "omgeven",
+  "oceaan": "diepte",
+  "stad": "urban",
+  "dorp": "klein",
+  "straat": "weg",
+  "plein": "open",
+  "vliegtuig": "lucht",
+  "trein": "spoor",
+  "metro": "ondergronds",
+  "tram": "rails",
+  "bus": "lijn",
+  "taxi": "rit",
+  "auto": "verkeer",
+  "fiets": "pedalen",
+  "step": "platform",
+  "skateboard": "wielen",
+  "helikopter": "rotor",
+  "raket": "ruimte",
+  "satelliet": "baan",
+  "ruimte": "kosmos",
+  "planeet": "baan",
+  "maan": "kraters",
+  "zon": "ster",
+  "ster": "lichtpunt",
+  "nevel": "gas",
+  "melkweg": "stelsel",
+  "universum": "oneindig",
+  "astronaut": "ruimtereis",
+  "dokter": "geneeskunde",
+  "ingenieur": "techniek",
+  "jurist": "recht",
+  "rechter": "oordeel",
+  "politie": "handhaving",
+  "brandweer": "blussen",
+  "piloot": "cockpit",
+  "bakker": "brood",
+  "kok": "keuken",
+  "boer": "landbouw",
+  "tuinier": "planten",
+  "monteur": "reparatie",
+  "elektricien": "stroom",
+  "loodgieter": "leidingen",
+  "schilder": "verf",
+  "kunstenaar": "creatie",
+  "muzikant": "klank",
+  "zanger": "stem",
+  "danser": "ritme",
+  "acteur": "rol",
+  "schrijver": "verhaal",
+  "journalist": "nieuws",
+  "fotograaf": "lens",
+  "programmeur": "code",
+  "designer": "ontwerp",
+  "architect": "structuur",
+  "onderzoeker": "data",
+  "wetenschapper": "theorie",
+  "psycholoog": "gedrag",
+  "accountant": "cijfers",
+  "bankier": "financiën",
+  "consultant": "advies",
+  "trainer": "coaching",
+  "coach": "leiding",
+  "lopen": "beweging",
+  "rennen": "snelheid",
+  "springen": "hoogte",
+  "zwemmen": "water",
+  "vliegen": "lucht",
+  "duiken": "diepte",
+  "drijven": "oppervlak",
+  "koken": "warmte",
+  "bakken": "oven",
+  "stoven": "langzaam",
+  "braden": "pan",
+  "snijden": "mes",
+  "schillen": "dun",
+  "lezen": "letters",
+  "schrijven": "tekst",
+  "rekenen": "cijfers",
+  "tekenen": "lijnen",
+  "zingen": "melodie",
+  "dansen": "ritme",
+  "lachen": "humor",
+  "huilen": "emotie",
+  "slapen": "rust",
+  "dromen": "fantasie",
+  "denken": "concept",
+  "leren": "kennis",
+  "studeren": "boeken",
+  "bouwen": "constructie",
+  "breken": "schade",
+  "openen": "toegang",
+  "sluiten": "einde",
+  "starten": "begin",
+  "stoppen": "pauze",
+  "helpen": "steun",
+  "geven": "overdracht",
+  "nemen": "verwerving",
+  "kopen": "betaling",
+  "verkopen": "handel",
+  "lenen": "tijdelijk",
+  "huren": "contract",
+  "liefde": "affectie",
+  "haat": "afkeer",
+  "vreugde": "blijdschap",
+  "verdriet": "sombaar",
+  "woede": "boosheid",
+  "rood": "kleur",
+  "blauw": "lucht",
+  "geel": "zon",
+  "groen": "natuur",
+  "paars": "mix",
+  "zwart": "donker",
+  "wit": "licht",
+  "goud": "edel",
+  "zilver": "metaal",
+  "bruin": "aarde",
+  "roze": "zacht",
+  "kat": "felis",
+  "hond": "canis",
+  "leeuw": "savanne",
+  "tijger": "strepen",
+  "olifant": "ivoor",
+  "giraf": "nek",
+  "panda": "bamboe",
+  "wolf": "roedel",
+  "vos": "sluw",
+  "beer": "bos",
+  "hert": "gewei",
+  "konijn": "sprong",
+  "muis": "klein",
+  "rat": "knager",
+  "egel": "stekels",
+  "eekhoorn": "noten",
+  "uil": "nacht",
+  "havik": "roofvogel",
+  "duif": "vleugel",
+  "merel": "zang",
+  "zwaan": "wit",
+  "kikker": "amfibie",
+  "pad": "wratten",
+  "slang": "schubben",
+  "schildpad": "pantser",
+  "haai": "vin",
+  "dolfijn": "intelligent",
+  "walvis": "reus",
+  "inktvis": "inkt",
+  "krab": "klauw",
+  "mier": "kolonie",
+  "bij": "honing",
+  "wesp": "angel",
+  "vlinder": "vleugels",
+  "kever": "pantser",
+  "computer": "processor",
+  "internet": "netwerk",
+  "wifi": "draadloos",
+  "server": "hosting",
+  "database": "tabellen",
+  "browser": "navigatie",
+  "website": "pagina",
+  "robot": "mechanisch",
+  "laser": "straal",
+  "sensor": "detectie",
+  "chip": "silicium",
+  "bit": "data",
+  "byte": "geheugen",
+  "software": "programma",
+  "hardware": "componenten",
+  "scherm": "display",
+  "speaker": "audio",
+  "projector": "beeld",
+  "printer": "afdruk"
+};
+
+
+
+const startButton = document.getElementById('start-button');
+const divInputFields = document.getElementById('input-fields');
+const name1 = document.getElementById('name1');
+const name2 = document.getElementById('name2');
+const name3 = document.getElementById('name3');
+const name4 = document.getElementById('name4');
+const name5 = document.getElementById('name5');
+const name6 = document.getElementById('name6');
+const name7 = document.getElementById('name7');
+const name8 = document.getElementById('name8');
+const name9 = document.getElementById('name9');
+const name10 = document.getElementById('name10');
+
+let players = [];
+
+let name1Value = '';
+let name2Value = '';
+let name3Value = '';
+let name4Value = '';
+let name5Value = '';
+let name6Value = '';
+let name7Value = '';
+let name8Value = '';
+let name9Value = '';
+let name10Value = '';
+
+const infoH1 = document.getElementById('info')
+const playerNow = document.getElementById('player-now')
+const showHideButton = document.getElementById('show-hide-button')
+const nextPlayerButton = document.getElementById('next-player-button')
+
+function start() {
+    setTimeout(function() {
+        startButton.style.display = 'none'
+        divInputFields.style.display = 'inline-block'
+    }, 1200)
+}
+
+let amount_of_players = 0
+let number_imposter = 0
+let full_word = []
+let keyword = ''
+let hint = ''
+
+function submit() {
+    setTimeout(function() {
+        players = [];
+        amount_of_players = 0;
+       
+        if (name1.value.trim() !== '') {
+            name1Value = name1.value;
+            players.push(name1Value);
+            amount_of_players++;
+        }
+        if (name2.value.trim() !== '') {
+            name2Value = name2.value;
+            players.push(name2Value);
+            amount_of_players++;
+        }
+        if (name3.value.trim() !== '') {
+            name3Value = name3.value;
+            players.push(name3Value);
+            amount_of_players++;
+        }
+        if (name4.value.trim() !== '') {
+            name4Value = name4.value;
+            players.push(name4Value);
+            amount_of_players++;
+        }
+        if (name5.value.trim() !== '') {
+            name5Value = name5.value;
+            players.push(name5Value);
+            amount_of_players++;
+        }
+        if (name6.value.trim() !== '') {
+            name6Value = name6.value;
+            players.push(name6Value);
+            amount_of_players++;
+        }
+        if (name7.value.trim() !== '') {
+            name7Value = name7.value;
+            players.push(name7Value);
+            amount_of_players++;
+        }
+        if (name8.value.trim() !== '') {
+            name8Value = name8.value;
+            players.push(name8Value);
+            amount_of_players++;
+        }
+        if (name9.value.trim() !== '') {
+            name9Value = name9.value;
+            players.push(name9Value);
+            amount_of_players++;
+        }
+        if (name10.value.trim() !== '') {
+            name10Value = name10.value;
+            players.push(name10Value);
+            amount_of_players++;
+        }
+
+        shuffle(players)
+
+        divInputFields.style.display = 'none'
+        showHideButton.style.display = 'inline-block'
+        nextPlayerButton.style.display = 'block'
+        nextPlayerButton.innerHTML = `Volgende: ${players[0]}`
+
+        full_word = getRandomItem(woorden)
+        keyword = full_word.key
+        hint = full_word.value
+
+        number_imposter = randomIntFromInterval(amount_of_players)
+        // console.log("Total players:", amount_of_players);
+        // console.log("Imposter index:", number_imposter);
+        // console.log("Players:", players);
+        // console.log("Keyword:", keyword);
+        // console.log("Hint:", hint);
+        changeButtonText();
+    }, 800)
+}
+let temp = 1
+
+
+function showhide() {
+    let hidden = true
+
+    if (temp <= amount_of_players) {
+
+        if (showHideButton.innerHTML == 'SHOW') {
+            showHideButton.innerHTML = 'HIDE'
+            hidden = false
+        } else if (showHideButton.innerHTML == 'HIDE') {
+            showHideButton.innerHTML = 'SHOW'
+            hidden = true
+        }
+
+
+        if (temp == number_imposter) {
+            infoH1.innerHTML = `!IMPOSTER! De tip is: ${hint}`
+            if (hidden) {
+                infoH1.style.display = 'none'
+            } else {
+                infoH1.style.display = 'block'
+            }
+        } else {
+            infoH1.innerHTML = `Het woord is: ${keyword}`
+            if (hidden) {
+                infoH1.style.display = 'none'
+            } else {
+                infoH1.style.display = 'block'
+            }
+        }
+    } else if (temp >= amount_of_players - 1) {
+        nextPlayerButton.innerHTML = 'Start The Game!'
+    } else if (temp >= amount_of_players) {
+        showHideButton.style.display = 'none'
+    }
+}
+
+let clicked = false
+
+nextPlayerButton.addEventListener('click', () => {
+    changeButtonText()
+    
+    if (temp >= amount_of_players - 1) {
+        nextPlayerButton.innerHTML = 'Start The Game!'
+    } else if (temp >= amount_of_players) {
+        showHideButton.style.display = 'none'
+    }
+
+    temp ++
+    infoH1.style.display = 'none'
+    showHideButton.innerHTML = 'SHOW'
+
+
+    if (nextPlayerButton.innerHTML == 'Start The Game!' && clicked == true) {
+        console.log("removed buttons")
+        nextPlayerButton.style.display = 'none'
+        showHideButton.style.display = 'none'
+        let beginPlayer = randomIntFromInterval(amount_of_players) - 1
+        infoH1.innerHTML = `${players[beginPlayer]} mag beginnen!`
+        playerNow.style.display = 'none'
+        infoH1.style.display = 'block'
+
+        setTimeout(() => {
+            revealButton.style.display = 'block'
+        }, 5000);
+    }
+
+    if (nextPlayerButton.innerHTML == 'Start The Game!') {
+        clicked = true
+    }
+
+
+})
+in_move = 1;
+function changeButtonText() {
+    nextPlayerButton.innerHTML = `Volgende: ${players[in_move]}`
+    playerNow.innerHTML = `Nu aan het kijken: ${players[in_move - 1]}`
+    in_move ++
+}
+
+
+function getRandomItem(obj) {
+    const keys = Object.keys(obj);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    return { key: randomKey, value: obj[randomKey] };
+}
+
+function randomIntFromInterval(max) { 
+  return Math.floor(Math.random() * max + 1);
+}
+
+const revealButton = document.getElementById('reveal')
+
+function revealTheImposter() {
+    revealButton.innerHTML = `${players[number_imposter - 1]}`
+}
+
+// Credits: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
